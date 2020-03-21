b93c13c36ed8ff53c18e29e8898d5511554a9324
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-03-21 03:17:53 -0300

    Added mouse functionality in HTML5 export

--- Scenes/Actors/Player.gd
+++ Scenes/Actors/Player.gd
@@ -2,30 +2,45 @@ extends Area2D
 
 signal hit
 
-export var speed: = 400
-
+export var speed = 400
 var screen_size
+# Add this variable to hold the clicked position.
+var target = Vector2()
 
-func _ready() -> void:
-	screen_size = get_viewport_rect().size
+func _ready():
 	hide()
+	screen_size = get_viewport_rect().size
 
 func start(pos):
 	position = pos
+	# Initial target is the start position.
+	target = pos
 	show()
 	$CollisionShape2D.disabled = false
 
-# warning-ignore:unused_argument
+# Change the target whenever a touch event happens.
+func _input(event):
+	if event is InputEventScreenTouch and event.pressed:
+		target = event.position
+
 func _process(delta):
-	var velocity = Vector2()  # The player's movement vector.
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
+	var velocity = Vector2()
+	# Move towards the target and stop when close.
+	if position.distance_to(target) > 10:
+		velocity = (target - position).normalized() * speed
+	else:
+		velocity = Vector2()
+
+# Remove keyboard controls.
+#    if Input.is_action_pressed("ui_right"):
+#       velocity.x += 1
+#    if Input.is_action_pressed("ui_left"):
+#        velocity.x -= 1
+#    if Input.is_action_pressed("ui_down"):
+#        velocity.y += 1
+#    if Input.is_action_pressed("ui_up"):
+#        velocity.y -= 1
+
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
@@ -33,21 +48,22 @@ func _process(delta):
 		$AnimatedSprite.stop()
 
 	position += velocity * delta
+	# We still need to clamp the player's position here because on devices that don't
+	# match your game's aspect ratio, Godot will try to maintain it as much as possible
+	# by creating black borders, if necessary.
+	# Without clamp(), the player would be able to move under those borders.
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "right"
 		$AnimatedSprite.flip_v = false
-		# See the note below about boolean assignment
 		$AnimatedSprite.flip_h = velocity.x < 0
-		
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
 
-
-func _on_Player_body_entered(body: Node) -> void:
+func _on_Player_body_entered( body ):
 	hide()
 	emit_signal("hit")
 	$CollisionShape2D.set_deferred("disabled", true)
