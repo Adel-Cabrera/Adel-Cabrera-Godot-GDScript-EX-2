9fa83fe556dbec454d3aa593b9d6416afa1d05e9
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-03-21 01:01:28 -0300

    Finished project

---
+++ Scenes/Actors/Mob.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+export var min_speed = 150  # Minimum speed range.
+export var max_speed = 250  # Maximum speed range.
+
+var mob_types = ["walk", "swim", "fly"]
+
+
+func _ready() -> void:
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+
+func _on_Visibility_screen_exited() -> void:
+	queue_free()
+	
+func _on_start_game():
+	queue_free()
